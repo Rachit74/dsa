4532256303ee7721df236dc94fd8a36bde0f53c0
rachit74 <rachithooda09@gmail.com>
2026-02-18 22:54:16 +0530

    go linked list

--- golang/linked_list/linked_list.go
+++ golang/linked_list/linked_list.go
@@ -48,6 +48,56 @@ func (l *LinkedList) Prepend(val int) {
 	l.Size++
 }
 
+func (l *LinkedList) InsertAtPosition(val int, pos int) {
+	// check out of bounds
+	if pos < 0 || pos > l.Size {
+		fmt.Println("Position out of index")
+		return
+	}
+
+	if pos == 0 {
+		l.Prepend(val)
+		return
+	}
+
+	if pos == l.Size {
+		l.Append(val)
+		return
+	}
+
+	// insert in the middle
+	newNode := &Node{Value: val}
+	current := l.Head
+
+	for i := 0; i < pos-1; i++ {
+		current = current.Next
+	}
+
+	newNode.Next = current.Next
+	current.Next = newNode
+
+	l.Size++
+}
+
+func (l *LinkedList) Search(val int) {
+	// if list is empty
+	if l.Head == nil {
+		fmt.Println("Empty list nothing to search")
+		return
+	}
+
+	current := l.Head
+	for i := 0; i < l.Size; i++ {
+		if current.Value == val {
+			fmt.Println("Traget found at index: ", i)
+			return
+		}
+		current = current.Next
+	}
+
+	fmt.Println("Target not found in the specified list")
+}
+
 // print list
 func (l *LinkedList) print() {
 	// check if list is empty
@@ -66,7 +116,11 @@ func (l *LinkedList) print() {
 func main() {
 	list := &LinkedList{}
 
-	list.Append(50)
-	list.Prepend(100)
+	list.Append(10)
+	list.Append(20)
+	list.Append(30)
+	list.Append(40)
+	list.InsertAtPosition(25, 3)
 	list.print()
+	list.Search(30)
 }
