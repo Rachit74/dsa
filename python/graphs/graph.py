2eddbf43f489431476800f774de39a3042976c2a
rachit74 <rachithooda09@gmail.com>
2026-02-21 15:32:25 +0530

    graph bfs dfs

--- python/graphs/graph.py
+++ python/graphs/graph.py
@@ -1,4 +1,6 @@
+from collections import deque
 
+# Adjacency List
 
 class Graph:
     def __init__(self):
@@ -15,12 +17,49 @@ class Graph:
             if not directed:
                 self.graph[v].append(u)
 
+    def bfs(self, start):
+         visited = set()
+         queue = deque([start])
+
+         while queue:
+              node = queue.popleft()
+              if node not in visited:
+                   visited.add(node)
+                   print(node, end=" ")
+                   queue.extend(self.graph[node])
+
+    def dfs(self, start, visited=None):
+        if visited is None:
+            visited = set()
+
+        visited.add(start)
+        print(start, end=" ")
+
+        for n in self.graph[start]:
+             if n not in visited:
+                  self.dfs(start=n, visited=visited)
+        
+
     def __str__(self):
         return str(self.graph)
     
-g1 = Graph()
-g1.add_vertex('A')
-g1.add_edge('A', 'B')
-g1.add_edge('B', 'C')
+    
+    
+g = Graph()
+g.add_vertex('A')
+g.add_edge('A', 'B')
+g.add_edge('A', 'C')
+g.add_edge('A', 'D')
+g.add_edge('A', 'E')
+g.add_edge('B', 'C')
+g.add_edge('B', 'F')
+g.add_edge('D', 'E')
+g.add_edge('D', 'G')
+g.add_edge('E', 'H')
 
-print(g1)
+print(g)
+
+g.bfs('A')
+print("\n")
+g.dfs('A')
+print("\n")
